c5f9b2bb27f45f8ead7afff0f175f6dfbe6d010d
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-01 16:05:39 +0500

    made changing in usage_middleware.py

--- services/api-gateway/app/middleware/usage_middleware.py
+++ services/api-gateway/app/middleware/usage_middleware.py
@@ -1,15 +1,13 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request
-from app.core.django_setup import django
-from usage.utils import log_api_usage  # import your Django logging function
+from app.core.redis_client import redis_client
+import asyncio
 
 class UsageMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         api_key = request.headers.get("X-API-Key")
         if api_key:
-            try:
-                log_api_usage(api_key, request.url.path, request.method)
-            except Exception as e:
-                print(f"Usage logging error: {e}")
+            key = f"usage:{api_key}:{request.url.path}"
+            await asyncio.to_thread(redis_client.incr, key)
         response = await call_next(request)
         return response
